3b0645816442f6b8435d0cd8795f05e26f4a5b91
Josh Reed <j.reed@realiefcenters.com>
2018-04-17 16:33:10 -0500

    Update ProductsService to use NitriteDb bean
    - Break logic out into several smaller methods
    - Add tests for all methods

--- grails-app/services/com/myretail/ProductsService.groovy
+++ grails-app/services/com/myretail/ProductsService.groovy
@@ -1,15 +1,68 @@
 package com.myretail
 
+import org.dizitart.no2.Document
+import org.dizitart.no2.filters.Filters
+
+import static org.dizitart.no2.Document.createDocument
+
 class ProductsService {
     static transactional = false
 
+    NitriteDb nitriteDb
+
     Map getProduct(long id) {
-        [id: id, name: 'Test', price: 123.45]
+        Map redsky = fetchRedskyProduct(id)
+        BigDecimal redskyPrice = redsky?.price
+
+        buildProduct(redsky, lookupLocalPrice(id, redskyPrice))
     }
 
     Map updatePrice(long id, BigDecimal price) {
-        // TODO: set price in NoSQL database
+        Map redsky = fetchRedskyProduct(id)
+        if (redsky) {
+            Document local = lookupLocalDocument(id)
+            if (local) {
+                local.price = price
+                nitriteDb.products.update(local)
+            } else {
+                nitriteDb.products.insert(createDocument('id', id).put('price', price))
+            }
+        }
 
-        getProduct(id)
+        buildProduct(redsky, price)
+    }
+
+    protected Map buildProduct(Map redsky, BigDecimal price) {
+        [
+                id   : redsky?.id,
+                name : redsky?.name,
+                price: price
+        ]
+    }
+
+    // NB: could probably cache this since only using name, which is unlikely to change for an id
+    protected Map fetchRedskyProduct(long id) {
+        // TODO: call Redsky API
+        // TODO: handle invalid id
+        [
+                id   : id,
+                name : 'Test',
+                price: 19.98
+        ]
+    }
+
+    protected BigDecimal lookupLocalPrice(long id, BigDecimal defaultPrice) {
+        // query the collection and use the first document if it exists
+        Document local = lookupLocalDocument(id)
+        if (local?.price) {
+            return local?.price
+        }
+
+        // otherwise return the default price
+        defaultPrice
+    }
+
+    protected Document lookupLocalDocument(long id) {
+        nitriteDb.products.find(Filters.eq('id', id)).find { true }
     }
 }
--- src/test/groovy/com/myretail/ProductsServiceSpec.groovy
+++ src/test/groovy/com/myretail/ProductsServiceSpec.groovy
@@ -1,25 +1,101 @@
 package com.myretail
 
 import grails.testing.services.ServiceUnitTest
+import org.dizitart.no2.Document
 import spock.lang.Specification
+
+import static org.dizitart.no2.Document.createDocument
 
 class ProductsServiceSpec extends Specification implements ServiceUnitTest<ProductsService> {
 
-    def setup() {
+    void "getProduct() combines data from Redsky API and local data store"() {
+        setup:
+        initializeNitriteDb([42: 9.99])
+
+        when: "non-existent id"
+        def product1 = service.getProduct(1)
+
+        then: "uses mocked price"
+        [id: 1, name: 'Test', price: 19.98] == product1
+
+        when: "existing id"
+        def product2 = service.getProduct(42)
+
+        then:
+        [id: 42, name: 'Test', price: 9.99] == product2
     }
 
-    def cleanup() {
+    void "updatePrice sets the price in the local data store"() {
+        setup:
+        initializeNitriteDb()
+
+        when: "no local price for id"
+        def product1 = service.getProduct(1)
+
+        then:
+        [id: 1, name: 'Test', price: 19.98] == product1
+
+        when: "set price for id"
+        def product2 = service.updatePrice(1, 9.99)
+
+        then:
+        [id: 1, name: 'Test', price: 9.99] == product2
     }
 
-    void "getProduct() returns mocked data"() {
+    void "fetchRedskyProduct() returns mocked data"() {
         expect: "mocked data"
-        service.getProduct(1) == [id: 1, name: 'Test', price: 123.45]
-        service.getProduct(42) == [id: 42, name: 'Test', price: 123.45]
+        service.fetchRedskyProduct(1) == [id: 1, name: 'Test', price: 19.98]
+        service.fetchRedskyProduct(42) == [id: 42, name: 'Test', price: 19.98]
     }
 
-    void "updatePrice() returns mocked data"() {
-        expect: "mocked data"
-        service.updatePrice(1, 0.00) == [id: 1, name: 'Test', price: 123.45]
-        service.updatePrice(42, 100.00) == [id: 42, name: 'Test', price: 123.45]
+    void "buildProduct() builds properly formatted Map"() {
+        expect:
+        service.buildProduct([id: 1234, name: 'Test'], 123.45) == [id: 1234, name: 'Test', price: 123.45]
+    }
+
+    void "lookupLocalDocument() returns null for ids that don't exist"() {
+        setup:
+        initializeNitriteDb()
+
+        expect: "null when id does not exists"
+        null == service.lookupLocalDocument(12345)
+    }
+
+    void "lookupLocalDocument() returns a document for ids that exist"() {
+        setup: "initialize in-memory NoSQL database"
+        initializeNitriteDb([42: 19.98])
+
+        when: "lookup with id that exists"
+        def document = service.lookupLocalDocument(42)
+
+        then: "document exists"
+        document?.price == 19.98
+    }
+
+    void "lookupLocalPrice() handles non-existing ids"() {
+        setup: "initialize in-memory NoSQL database"
+        initializeNitriteDb([42: 19.98])
+
+        expect: "default price when id does not exist"
+        9.99 == service.lookupLocalPrice(12345, 9.99)
+    }
+
+    void "lookupLocalPrice() handles existing ids"() {
+        setup: "initialize in-memory NoSQL database"
+        initializeNitriteDb([42: 19.99])
+
+        expect: "stored price when id does exist"
+        19.99 == service.lookupLocalPrice(42, null)
+    }
+
+    protected initializeNitriteDb(Map products = [:]) {
+        service.nitriteDb = new NitriteDb()
+        service.nitriteDb.init()
+
+        def collection = service.nitriteDb.products
+
+        products.each { id, price ->
+            collection.insert(createDocument('id', (id as long)).put('price', price))
+        }
     }
 }
